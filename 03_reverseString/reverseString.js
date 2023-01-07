c8416dd2760948697e3272d40e096cdd96aac1dc
Pie315 <devonkaelberer@gmail.com>
2023-01-07 14:44:19 -0500

    completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(inputStr) {
+    let output = "";
 
+    for (let i = inputStr.length -1; i >= 0; i--) {
+        output += inputStr[i];
+    }
+
+    return output;
 };
 
 // Do not edit below this line
