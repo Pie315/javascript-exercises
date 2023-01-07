05da851dce952653fe57d09bc7f20b393b7f46c7
Pie315 <devonkaelberer@gmail.com>
2023-01-07 14:30:14 -0500

    finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(inputStr, count) {
+    if (count < 0) {
+        return "ERROR";
+    }
+    
+    let out = "";
+    for(let i = 0; i < count; i++) {
+        out += inputStr;
+    }
+    return out;
 };
 
 // Do not edit below this line
