ac8d16ae7bc6e508faeb54da4b4a73bf1e861095
Pie315 <devonkaelberer@gmail.com>
2023-01-08 15:00:29 -0500

    complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(FahrenheitIn) {
+  let celsiusOut = ((FahrenheitIn -32) * (5/9));
+  celsiusOut = ((Math.round(celsiusOut * 10))/10);
+  return celsiusOut;
+}
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(CelsiusIn) {
+  let fahrenheitOut = ((CelsiusIn * (9/5)) + 32);
+  return ((Math.round(fahrenheitOut * 10))/10);
+}
 
 // Do not edit below this line
 module.exports = {
