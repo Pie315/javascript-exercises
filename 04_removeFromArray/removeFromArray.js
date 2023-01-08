4559bb0620e16bb90dc1247b16541a41650f1124
Pie315 <devonkaelberer@gmail.com>
2023-01-08 13:29:28 -0500

    completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(origList, ...toBeRemoved) { // toBeRemoved is a list
+    
+    let returnList = origList;
+    for (let index in toBeRemoved) { // loops for each item ro be removed
+        for(let listPosition in origList) { // loops through list
+            if (toBeRemoved[index] === origList[listPosition]) {
+                returnList.splice(listPosition,1);
+            }
+        }
+    } return returnList;
 };
 
 // Do not edit below this line
