750afa6e7aeeff5fb82cfe57dd8ba1fe74eee5c9
Pie315 <devonkaelberer@gmail.com>
2023-01-18 13:42:39 -0500

    complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(...numbers) {
+	let out = numbers.reduce((total, numbers) => {
+    return total + numbers;
+  }, 0);
+  return out;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, ...numbers) {
+  let out = numbers.reduce((total, numbers) => {
+    return total - numbers;
+  }, 0);
+  return out + a;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let out = numbers.reduce((total, numbers) => {
+    return total + numbers;
+  }, 0);
+  return out;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	let out = array.reduce((total, numbers) => {
+    return total * numbers;
+  });
+  return out;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0) {
+    return 1;
+  }
+  
+  for (i = a - 1; i > 1; i--) {
+    a *= i;
+  }
+  return a;
 };
 
 // Do not edit below this line
